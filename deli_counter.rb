5aefa6f6cab18c79b49ca770d9d326a20c57c142
David Kuruvilla <dapawn@gmail.com>
2017-10-18 19:50:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+
+def line(arr)
+  s = "The line is currently:"
+  arr.length == 0 ? p return "The line is currently empty." : arr.each_with_index{ |x, i| s << " #{i+1}. #{x}" }
+  return s
+end
+
+def take_a_number(arr, name)
+
+end
+
+def now_serving(arr)
+
+end
