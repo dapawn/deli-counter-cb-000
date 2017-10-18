19ce59e308a73659a76a1af279d41f2011810de3
David Kuruvilla <dapawn@gmail.com>
2017-10-18 20:12:45 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,5 +12,5 @@ def take_a_number(arr, name)
 end
 
 def now_serving(arr)
-  arr.length == 0 ? s = "Hi" : s = "Currently serving #{arr.pop}."
+  arr.length == 0 ? s = "Hi" : puts "Currently serving #{arr.pop}."
 end
